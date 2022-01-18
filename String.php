0d4eb539d696d0744756cd316881d47383ba542c
Jahir Uddin Mahmud <97908440+Jahir361@users.noreply.github.com>
2022-01-18 10:42:02 +0600

    Create String.php

---
+++ String.php
@@ -0,0 +1,47 @@
+<?php
+// $fname = "Mohiuddin";
+// $lname = "Tarek";
+// $name = "Mohiuddin tarek";
+// echo $fname," ",$lname . "</br>";
+// echo $fname. " " . $lname;
+// // to dislocat the name.....
+// echo strrev($name). "</br>";
+
+// // to calculate the lanth of name '
+// echo strlen($name);
+
+echo "Enter your ". "</br>";
+echo "Fast Name". "</br>";
+echo "Mid Name". "</br>";
+echo "Last name". "</br>";
+
+
+$fast_name = "jahir";
+$Mid_name = "Uddin";
+$Last_name = "Mahmud";
+$name = "Jahir Uddin Mahmud";
+$random = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quam orci, ultrices ut eros id, aliquet pulvinar ante. Nulla pulvinar, orci nec aliquet fermentum, magna nulla blandit augue, quis efficitur dui purus at ante. Etiam eu tortor sit amet ipsum commodo ullamcorper. Ut non quam ut tellus tincidunt posuere. Vivamus cursus nulla quis nulla fermentum vehicula. Mauris eros massa, blandit et molestie at, faucibus sed ipsum. In ut ex vitae dui dictum tristique. Duis sed nisi et quam tempor aliquam. Vivamus lacinia orci enim, pellentesque pretium enim tincidunt non. In vitae consectetur enim. Etiam nibh urna, efficitur in ex vitae, pellentesque auctor nisl. Maecenas posuere lacus metus, aliquet porta libero luctus eu. Pellentesque vitae tempus tortor, at congue mauris. Proin eu pulvinar neque. Praesent volutpat, lorem et feugiat egestas, quam odio dictum lorem, at consectetur nisi diam vitae purus. Morbi semper rhoncus mi, a ultrices magna mollis non.";
+
+
+echo "YOUR NAME IS ". "</br>";
+echo $fast_name," ",$Mid_name," ", $Last_name. "</br>";
+
+echo "YOUR NAME IS ". "</br>";
+echo $fast_name." ".$Mid_name." ". $Last_name. "</br>";
+
+echo strlen ($name)."</br>"; 
+
+echo "SORRY PAY FASY FOR THE SERVICE ". "</br>";
+echo strrev($name). "</br>";
+
+echo "</br>";
+echo "</br>";
+
+
+echo str_shuffle ($random). "</br>";
+
+echo "</br>";
+echo "</br>";
+ 
+echo "THANK YOU....😊"
+ ?>
